5fc1bef8ffca101d92b8034e5c82fa0343b51d2d
brianbrowndev <brbrowngeo@gmail.com>
2022-01-04 16:39:18 -0500

    pipeline: nginx refresh

--- src/index.tsx
+++ src/index.tsx
@@ -5,7 +5,7 @@ import App from "./App";
 import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(
-  <Router basename="me">
+  <Router basename="/me">
     <App />
   </Router>,
   document.getElementById("root")
