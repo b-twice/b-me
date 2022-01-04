616bbd7126168021e2f05780aa57773599a0c2a1
brianbrowndev <brbrowngeo@gmail.com>
2022-01-04 15:00:37 -0500

    pipeline: nginx refresh

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,22 @@
-import React from "react";
-
-import { BrowserRouter as Router } from "react-router-dom";
 import { AuthProvider } from "./core/Auth";
 
 import { BlogProvider } from "./blog/BlogContext";
 import { CssBaseline } from "@material-ui/core";
 import Main from "./Main";
 import { AppThemeContextProvider } from "./theme/AppThemeContext";
+import { Fragment } from "react";
 
 function App() {
   return (
     <AppThemeContextProvider>
-      <Router basename="/me/">
+      <Fragment>
         <CssBaseline />
         <AuthProvider>
           <BlogProvider>
             <Main></Main>
           </BlogProvider>
         </AuthProvider>
-      </Router>
+      </Fragment>
     </AppThemeContextProvider>
   );
 }
